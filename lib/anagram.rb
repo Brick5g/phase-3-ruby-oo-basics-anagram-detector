03666ebe85904cffb70096e1c8a55a56943712d8
Julian Francis <jgfrancis3@gmail.com>
2026-07-11 11:30:58 -0400

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    possible_anagrams.select do |possible_anagram|
+      possible_anagram.chars.sort == @word.chars.sort
+    end
+  end
+end
